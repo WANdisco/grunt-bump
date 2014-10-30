a4bbd8044aba3f019fa67bb2e18a021d214c627a
Evelina Agostini <evelina.agostini@elbuild.it>
2014-10-30 09:48:27 +0000

    Added test to commit to avoid fatal error if no staged files present

--- tasks/bump.js
+++ tasks/bump.js
@@ -151,13 +151,23 @@ module.exports = function(grunt) {
     runIf(opts.commit, function() {
       var commitMessage = opts.commitMessage.replace('%VERSION%', globalVersion);
 
-      exec('git commit ' + opts.commitFiles.join(' ') + ' -m "' + commitMessage + '"', function(err, stdout, stderr) {
-        if (err) {
-          grunt.fatal('Can not create the commit:\n  ' + stderr);
+      //check if there are staged files
+      exec ('git diff --cached', function(err, stdout, stderr) {
+        if (stdout){
+          exec('git commit ' + opts.commitFiles.join(' ') + ' -m "' + commitMessage + '"', function(err, stdout, stderr) {
+          if (err) {
+            grunt.fatal('Can not create the commit:\n  ' + stderr);
+          }
+          grunt.log.ok('Committed as "' + commitMessage + '"');
+          });
         }
-        grunt.log.ok('Committed as "' + commitMessage + '"');
+        else{
+          grunt.log.ok('No changes to commit');
+        }
         next();
       });
+
+
     });
 
 
